660d40fd6e91877d158103680f4da5261b985583
thusitha madusanka <thusitha madusanka>
2016-06-26 19:27:28 +0530

    connect to the database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //configaration file, adding some middleware
 module.exports = {
-	"database": "",
+	"database": "mongodb://root:abc123@ds023684.mlab.com:23684/mynewuserstroy",
 	"port": process.env.PORT || 3000,
 	"secretKey": "yoursecretkey"
 	//you can use this in serverJs
--- server.js
+++ server.js
@@ -3,15 +3,29 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var config = require("./config");
+var mongoose = require("mongoose");
+
+//create new instance of express object
+var app = express();
+
+//call database
+mongoose.connect(config.database, function(err){
+	if (err) {
+		console.log(err);
+	}else{
+		console.log("Connect to the database");
+	}
+});
 
 //add the middleware
 app.use(bodyParser.urlencoded({	extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.get("*", function(req, res){
+	res.sendFile(__dirname + '/public/view/index.html');
+});
 
-//create new instance of express object
-var app = express();
 
 app.listen(config.port, function(err){
 	if (err) {
